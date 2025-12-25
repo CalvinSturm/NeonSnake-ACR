489cf7ab20f97fa87a9101c7b0ab7354c96a1b21
Calvin Sturm <calvinsturm@gmail.com>
2025-12-24 22:05:13 -0800

    chore: remove compiled JS artifacts and enforce TypeScript-only source

--- App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import SnakeGame from './components/SnakeGame';
-const App = () => {
-    return (_jsx("div", { className: "h-screen w-screen bg-black overflow-hidden flex items-center justify-center", children: _jsx(SnakeGame, {}) }));
-};
-export default App;
